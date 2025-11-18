a794c3a0082264bb4640dc6ad8082c414e7be1fd
moritzalbert1 <hello@moritzalbert.com>
2025-11-18 11:08:10 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Custom cursor für alle Seiten
-const cursor = document.querySelector(".custom-cursor");
-
-if (cursor) {
-    document.addEventListener("mousemove", e => {
-        cursor.style.top = e.clientY + "px";
-        cursor.style.left = e.clientX + "px";
-    });
-}
